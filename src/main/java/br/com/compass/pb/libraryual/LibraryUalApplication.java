67c26245c044f14072a1f812f565e0c6877e9d10
Thiago Odilon <thiago-almeida05@hotmail.com>
2023-07-10 15:34:46 -0300

    :tada: first commit

---
+++ src/main/java/br/com/compass/pb/libraryual/LibraryUalApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.libraryual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryUalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryUalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compass/pb/libraryual/LibraryUalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.libraryual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryUalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
