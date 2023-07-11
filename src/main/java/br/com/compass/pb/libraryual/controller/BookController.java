03a74b8bf656c009d8fc42748db3b2db5bafbc3b
= <=>
2023-07-11 20:14:40 -0300

    CRUD book

--- src/main/java/br/com/compass/pb/libraryual/controller/BookController.java
+++ src/main/java/br/com/compass/pb/libraryual/controller/BookController.java
@@ -1,4 +1,39 @@
 package br.com.compass.pb.libraryual.controller;
 
+import br.com.compass.pb.libraryual.model.Book;
+import br.com.compass.pb.libraryual.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/book")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/")
+    public List<Book> findAll(){
+        return bookService.findAll();
+    }
+
+    @PostMapping("/")
+    public Book insert(@RequestBody Book object){
+        return bookService.insert(object);
+    }
+
+    @PutMapping("/")
+    public Book update(@RequestBody Book object){
+        return bookService.update(object);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
+        bookService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
